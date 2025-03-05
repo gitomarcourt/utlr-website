080f120d845f949d7df2d2b207d05075a7152f35
tom <arcourt.tom@gmail.com>
2025-03-05 20:34:23 +0100

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,8 @@
           muted
           playsinline
         >
-          <source src="../assets/videos/hero-background.webm" type="video/mp4">
+          <source src="../assets/videos/hero-background.webm" type="video/webm">
+          <source src="../assets/videos/hero-background.mp4" type="video/mp4">
         </video>
       </div>
 
